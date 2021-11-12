be687a6dbf76b190288224dfe3e77d0d29e9c89b
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-11 17:44:42 -0700

    created controller for jewelry

---
+++ controllers/jewelry-controller.js
@@ -0,0 +1,45 @@
+const { Jewelry } = require("../models");
+
+const jewelryController = {
+  // get all jewelry
+  getAllJewelry(req, res) {
+    Jewelry.find({})
+      .then((dbJewelryData) => res.json(dbJewelryData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one jewelry by id
+  getJewelryById({ params }, res) {
+    Jewelry.findOne({ _id: params.id })
+      .then((dbJewelryData) => res.json(dbJewelryData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  //create Jewelry log
+  createJewelryLog({ body }, res) {
+    Jewelry.create(body)
+      .then((dbJewelryData) => res.json(dbJewelryData))
+      .catch((err) => res.json(err));
+  },
+
+  //Update Jewelry Log
+  updateJewelry({ params, body }, res) {
+    Jewelry.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbJewelryData) => {
+        if (!dbJewelryData) {
+          res.status(404).json({ message: "No jewlery log with this id." });
+          return;
+        }
+        res.json(dbJewelryData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = jewelryController;
